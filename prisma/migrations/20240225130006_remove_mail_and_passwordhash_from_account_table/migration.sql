8c4796eb5b6276e318ed1d2d60e889f15bc9c693
yamakeeeeeeeeen <yamakeeeeeeeeen@gmail.com>
2024-02-25 22:00:44 +0900

    chore: Accountからmailとpasswordhashを一旦消す

---
+++ prisma/migrations/20240225130006_remove_mail_and_passwordhash_from_account_table/migration.sql
@@ -0,0 +1,13 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `email` on the `Account` table. All the data in the column will be lost.
+  - You are about to drop the column `passwordHash` on the `Account` table. All the data in the column will be lost.
+
+*/
+-- DropIndex
+DROP INDEX "Account_email_key";
+
+-- AlterTable
+ALTER TABLE "Account" DROP COLUMN "email",
+DROP COLUMN "passwordHash";
