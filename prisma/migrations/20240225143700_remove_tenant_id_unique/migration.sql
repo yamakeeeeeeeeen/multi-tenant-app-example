32cdf8f634b4d4e77cb386bf64497d8760beb63e
yamakeeeeeeeeen <yamakeeeeeeeeen@gmail.com>
2024-02-26 19:59:25 +0900

    feat: migrate

---
+++ prisma/migrations/20240225143700_remove_tenant_id_unique/migration.sql
@@ -0,0 +1,14 @@
+-- DropIndex
+DROP INDEX "Account_tenantId_key";
+
+-- DropIndex
+DROP INDEX "Employee_tenantId_key";
+
+-- DropIndex
+DROP INDEX "EmployeeAccount_tenantId_key";
+
+-- DropIndex
+DROP INDEX "User_tenantId_key";
+
+-- DropIndex
+DROP INDEX "UserAccount_tenantId_key";
