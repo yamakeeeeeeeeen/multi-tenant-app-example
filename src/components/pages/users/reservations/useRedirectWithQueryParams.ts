7f1946f533b85dd44578b5c7b20e18fdc3bac86a
yamakeeeeeeeeen <yamakeeeeeeeeen@gmail.com>
2024-03-10 19:57:29 +0900

    feat: Refactor useRedirectWithYearAndMonth to validate year and month

--- src/components/pages/users/reservations/useRedirectWithQueryParams.ts
+++ src/components/pages/users/reservations/useRedirectWithQueryParams.ts
@@ -1,12 +1,17 @@
 import { usePathname, useRouter } from 'next/navigation'
 import { useEffect } from 'react'
 
+const isValidYearAndMonth = (year: number, month: number): boolean => {
+  const date = new Date(year, month - 1)
+  return date.getFullYear() === year && date.getMonth() === month - 1
+}
+
 export const useRedirectWithYearAndMonth = (year: number, month: number) => {
   const router = useRouter()
   const pathname = usePathname()
 
   useEffect(() => {
-    if (!year || !month) {
+    if (!isValidYearAndMonth(year, month)) {
       const now = new Date()
       const queryString = new URLSearchParams({
         year: now.getFullYear().toString(),
