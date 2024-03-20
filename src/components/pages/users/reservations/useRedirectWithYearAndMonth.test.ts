b65c23500df39ac81c02ad5c64b23112cbeaf61b
yamakeeeeeeeeen <yamakeeeeeeeeen@gmail.com>
2024-03-21 08:54:21 +0900

    Add unit tests for useRedirectWithYearAndMonth

---
+++ src/components/pages/users/reservations/useRedirectWithYearAndMonth.test.ts
@@ -0,0 +1,41 @@
+import { renderHook } from '@testing-library/react'
+import { usePathname, useRouter } from 'next/navigation'
+
+import { useRedirectWithYearAndMonth } from './useRedirectWithQueryParams'
+
+jest.mock('next/navigation', () => ({
+  useRouter: jest.fn(),
+  usePathname: jest.fn(),
+}))
+
+describe('useRedirectWithYearAndMonth', () => {
+  beforeEach(() => {
+    jest.clearAllMocks()
+  })
+
+  it('有効な年と月が与えられた場合、リダイレクトが発生しないこと', () => {
+    const mockReplace = jest.fn()
+    ;(useRouter as jest.Mock).mockReturnValue({ replace: mockReplace })
+    ;(usePathname as jest.Mock).mockReturnValue('/test')
+
+    renderHook(() => useRedirectWithYearAndMonth(2023, 5))
+
+    expect(mockReplace).not.toHaveBeenCalled()
+  })
+
+  it('無効な年と月が与えられた場合、現在の年と月を使用してリダイレクトが発生すること', () => {
+    const mockReplace = jest.fn()
+    ;(useRouter as jest.Mock).mockReturnValue({ replace: mockReplace })
+    ;(usePathname as jest.Mock).mockReturnValue('/test')
+
+    const now = new Date()
+    const expectedYear = now.getFullYear()
+    const expectedMonth = now.getMonth() + 1
+    const expectedQueryString = `year=${expectedYear}&month=${expectedMonth}`
+    const expectedRedirectUrl = `/test?${expectedQueryString}`
+
+    renderHook(() => useRedirectWithYearAndMonth(2023, 13))
+
+    expect(mockReplace).toHaveBeenCalledWith(expectedRedirectUrl)
+  })
+})
