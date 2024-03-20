cbaf13f0e11e4bd2825ba8fbf9774693f61bdd5f
yamakeeeeeeeeen <yamakeeeeeeeeen@gmail.com>
2024-03-20 18:00:58 +0900

    Add comment

--- src/components/pages/users/reservations/useCalendar.ts
+++ src/components/pages/users/reservations/useCalendar.ts
@@ -4,6 +4,7 @@ export const useCalendar = (year: number, month: number) => {
   const endDate = new Date(year, month, 0)
 
   // 月の初日が週のどの曜日から始まるかを計算
+  // 日曜日: 0, 月曜日: 1, ..., 土曜日: 6
   const startDayOfWeek = startDate.getDay()
 
   // 月の開始前と終了後の日付を計算
